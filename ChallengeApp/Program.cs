a314404d6771606bb31aac27b1fa9d93bdbe7f5d
jarczak <rafal.kraczaj@gmail.com>
2023-03-01 18:42:55 +0100

    Zadanie dzień 3

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "kobieta";
+var age = 33;
+Console.WriteLine(name + "" + sex + "" + age);
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta, poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine(name + " lat " + age);
+}
+
+if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
