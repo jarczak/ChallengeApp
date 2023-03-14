44fe5979b270c5c2a754fe82cd8f9d18e2324ab5
jarczak <rafal.kraczaj@gmail.com>
2023-03-14 08:25:49 +0100

    Dzień 10

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -13,7 +13,46 @@ public class Employee
 
     public void AddGrade (float grade)
     {
-        grades.Add(grade);
+        if (grade >= 0 && grade <= 100)
+        {
+            grades.Add(grade);
+        }
+        else
+        {
+            Console.WriteLine("Invalid grade value!");
+        }
+        
+    }
+    public void AddGrade (string grade)
+    {
+        if(float.TryParse(grade, out float value))
+        {
+            this.AddGrade(value);
+        }
+        else
+        {
+            Console.WriteLine("String is not float!");
+        }
+    }
+    public void AddGrade (int grade)
+    {
+        this.AddGrade((float)grade);
+    }
+    public void AddGrade (double grade)
+    {
+        this.AddGrade((float)grade);
+    }
+    public void AddGrade (byte grade)
+    {
+        this.AddGrade((float)grade);
+    }
+    public void AddGrade (long grade)
+    {
+        this.AddGrade((float)grade);
+    }
+    public void AddGrade (short grade)
+    {
+        this.AddGrade((float)grade);
     }
 
     public Statistics GetStatistics ()
