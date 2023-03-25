4fc4457395c2fcae94f3dbd0c2d5dd07e79b513a
jarczak <rafal.kraczaj@gmail.com>
2023-03-25 16:28:54 +0100

    Dzień 16

---
+++ ChallengeApp/EmployeeInMemory.cs
@@ -0,0 +1,96 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInMemory : EmployeeBase
+    {
+        private List<float> grades = new List<float>();
+        public EmployeeInMemory(string name, string surname) 
+            : base(name, surname)
+        {
+        }
+        public override void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("Invalid grade value!");
+            }
+
+        }
+
+        public override void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float value))
+            {
+                this.AddGrade(value);
+            }
+            else
+            {
+                throw new Exception("String is not float!");
+            }
+        }
+
+        public override void AddGrade(char grade)
+        {
+            switch (grade)
+            {
+                case 'A':
+                case 'a':
+                    this.grades.Add(80);
+                    break;
+                case 'B':
+                case 'b':
+                    this.grades.Add(60);
+                    break;
+                case 'C':
+                case 'c':
+                    this.grades.Add(40);
+                    break;
+                case 'D':
+                case 'd':
+                    this.grades.Add(20);
+                    break;
+                default:
+                    throw new Exception("Wrong letter!");
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Average = 0;
+            foreach (var grade in grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+            statistics.Average /= grades.Count;
+
+            switch (statistics.Average)
+            {
+                case var average when average >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var average when average >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var average when average >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var average when average >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+
+            return statistics;
+        }
+    }
+}
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,14 +1,20 @@
-﻿Console.WriteLine("Witamy w programie XYZ do oceny pracownika!");
+﻿using ChallengeApp;
+
+Console.WriteLine("Witamy w programie XYZ do oceny pracownika!");
 Console.WriteLine("===========================================");
 Console.WriteLine();
-var emp = new Employee("Rafał", "Kraczaj", "M");
+var emp = new EmployeeInFile("Rafał", "Kraczaj");
+emp.AddGrade(3.6f);
+emp.AddGrade('D');
+emp.AddGrade(34);
+emp.AddGrade(10f);
 
 
-while (true)
+/*while (true)
 {
     Console.WriteLine("Podaj ocenę pracownika: (podaj q aby zakończyć dodawanie)");
     var input = Console.ReadLine();
-    if(input == "q" || input == "Q")
+    if (input == "q" || input == "Q")
     {
         break;
     }
@@ -16,11 +22,11 @@ while (true)
     {
         emp.AddGrade(input);
     }
-    catch(Exception e) 
+    catch (Exception e)
     {
         Console.WriteLine($"An exception occured: {e}");
     }
-}
+}*/
 var stat = emp.GetStatistics();
 Console.WriteLine($"Statystyki dla pracownika: {emp.Name}, {emp.Surname}");
 Console.WriteLine($"Średnia ocen: {stat.Average}");
